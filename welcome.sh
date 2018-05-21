ae63cf345f2477d4f0e6478bb6e078f2c320c9bc
Samuel Piecz <samuel.piecz@gmail.com>
2018-05-21 02:12:56 -0500

    Automated development environment setup for CorrelatePro.

---
+++ welcome.sh
@@ -0,0 +1,21 @@
+#!/bin/sh 
+
+DEV_PATH="~/Projects/CorrelatePro"
+SESSION="CorrelatePro"
+tmux -2 new-session -d -s $SESSION
+tmux send-keys "cd $DEV_PATH; workon correlate_pro; clear" C-m
+tmux split-window -v
+tmux select-pane -t 1
+tmux send-keys "VBoxManage controlvm 'dev' poweroff" C-m
+tmux send-keys "clear" C-m
+tmux send-keys "cd $DEV_PATH" C-m
+tmux send-keys "clear" C-m
+tmux send-keys "VBoxManage startvm 'dev' --type headless" C-m
+tmux send-keys "clear" C-m
+tmux send-keys "workon correlate_pro" C-m
+tmux send-keys "clear" C-m
+tmux send-keys "git pull origin master" C-m
+tmux send-keys "eval \$(docker-machine env dev)" C-m
+tmux send-keys "docker-compose -f local.yml build" C-m
+tmux send-keys "docker-compose -f local.yml up" C-m
+tmux -2 attach-session -t $SESSION
