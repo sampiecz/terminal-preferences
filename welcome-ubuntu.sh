ff23aadfa4042ee7b263e2f91b724679036d05ad
sampiecz <samuel.piecz@gmail.com>
2018-09-03 22:49:24 -0500

    Updated welcome-ubuntu script to resize windows appropriately for obs.

--- welcome-ubuntu.sh
+++ welcome-ubuntu.sh
@@ -1,10 +1,13 @@
 #!/bin/sh 
-
+killall tmux
 DEV_PATH="~/Projects/CorrelatePro"
 SESSION="CorrelatePro"
 tmux -2 new-session -d -s $SESSION
 tmux send-keys "cd $DEV_PATH; workon CorrelatePro; clear" C-m
+tmux send-keys "git pull origin master" C-m
 tmux split-window -v
+tmux select-pane -t 0
+tmux resize-pane -D 11
 tmux select-pane -t 1
 tmux send-keys "VBoxManage controlvm 'dev' poweroff" C-m
 tmux send-keys "clear" C-m
@@ -14,7 +17,6 @@ tmux send-keys "VBoxManage startvm 'dev' --type headless" C-m
 tmux send-keys "clear" C-m
 tmux send-keys "workon CorrelatePro" C-m
 tmux send-keys "clear" C-m
-tmux send-keys "git pull origin master" C-m
 tmux send-keys "eval \$(docker-machine env dev)" C-m
 tmux send-keys "sudo docker-compose -f local.yml build" C-m
 tmux send-keys "sudo docker-compose -f local.yml up" C-m
