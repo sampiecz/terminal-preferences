582eed351f146f0f7c7b880b19801c94381344a4
sampiecz <samuel.piecz@gmail.com>
2018-12-06 15:37:24 -0600

    New 18.04 configs.

--- welcome-ubuntu.sh
+++ welcome-ubuntu.sh
@@ -4,7 +4,6 @@ DEV_PATH="~/Projects/CorrelatePro"
 SESSION="CorrelatePro"
 tmux -2 new-session -d -s $SESSION
 tmux send-keys "cd $DEV_PATH; workon CorrelatePro; clear" C-m
-tmux send-keys "git pull origin master" C-m
 tmux split-window -v
 tmux select-pane -t 0
 tmux resize-pane -D 11
@@ -18,6 +17,8 @@ tmux send-keys "clear" C-m
 tmux send-keys "workon CorrelatePro" C-m
 tmux send-keys "clear" C-m
 tmux send-keys "eval \$(docker-machine env dev)" C-m
-tmux send-keys "sudo docker-compose -f local.yml build" C-m
-tmux send-keys "sudo docker-compose -f local.yml up" C-m
+tmux send-keys "docker-machine ls" C-m
+#tmux send-keys "sudo docker-compose -f local.yml build" C-m
+tmux send-keys "b && u" C-m
+#tmux send-keys "sudo docker-compose -f local.yml up" C-m
 tmux -2 attach-session -t $SESSION
